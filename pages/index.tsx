048947c94850bde9e839771be74d3466a81474c9
Tathagata Roy <tathagata.roy1278@gmail.com>
2023-09-15 23:23:30 +0530

    Fixed post bug

---
+++ pages/index.tsx
@@ -0,0 +1,43 @@
+import type { GetServerSideProps, NextPage } from "next";
+import Head from "next/head";
+import { Toaster } from "react-hot-toast";
+import Feed from "../components/Feed";
+import Sidebar from "../components/Siderbar";
+import Widgets from "../components/Widgets";
+import { Tweet } from "../typing";
+import { fetchTweets } from "../utils/fetchTweets";
+
+interface Props {
+  tweets: Tweet[];
+}
+
+const Home = ({ tweets }: Props) => {
+  console.log(tweets);
+  return (
+    <div className="max-h-screen overflow-hidden lg:max-w-6xl">
+      <Head>
+        <title>Twitter Clone</title>
+      </Head>
+      <Toaster />
+
+      <main className="grid grid-cols-9">
+        <Sidebar />
+
+        <Feed tweets={tweets} />
+
+        <Widgets />
+      </main>
+    </div>
+  );
+};
+
+export default Home;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const tweets = await fetchTweets();
+  return {
+    props: {
+      tweets,
+    },
+  };
+};
